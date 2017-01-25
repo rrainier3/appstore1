7bf4f504d3dee274291ffdbe881b604dba3227fd
Ray Rainier <rayrainier@yahoo.com>
2017-01-25 17:10:35 -0600

    class FeaturedAppsController: UICollectionViewController

--- appstore1/FeaturedAppsController.swift
+++ appstore1/FeaturedAppsController.swift
@@ -10,13 +10,47 @@ import UIKit
 
 class FeaturedAppsController: UICollectionViewController {
 
+	private let cellId = "cellId"
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        collectionView?.backgroundColor = .red
+        collectionView?.backgroundColor = .white
+        
+        collectionView?.register(CategoryCell.self, forCellWithReuseIdentifier: cellId)
         
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        
+        return cell
+    }
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 3
+    }
 
+}
+
+class CategoryCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupViews() {
+        
+    }
+    
 }
 
